12f67408ce208b3e9eed5e2025e316b46f33c8c8
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-09-11 18:21:20 +0300

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -11,19 +11,29 @@ class Scanner {
     QList<QString> scannedNetworks{};
     QList<QString> activeHosts{};
     QMap<QString,QString> hostsOS{};
-    QMap<QString,QString> hostsPorst{};
+    QMap<QString,QString> hostsPorts{};
+
+    const QList<quint32> defaultSYNPorts = {22, 135, 139, 445};
 
 public:
     Scanner();
+    Scanner(const QList<QString>& scannedNetworks);
+
+    QList<QString> getScannedNetworks();
+    QList<QString> getActiveHosts();
+    QMap<QString,QString> getHostsOS();
+    QMap<QString,QString> getHostsPorts();
 
     void detectActiveHostsICMP();
     void detectActiveHostsARP();
-    void detectActiveHostsACK();
+    void detectActiveHostsSYN();
 
     QList<quint32> detectHostOpenPorts(QHostAddress ipAddress, QList<quint32> ports);
     QString getServiceName(QString ipAddress, quint32 port);
+    QList<QString> getNetworksHosts();
 
-    static QMap<QString, QString> getCurrentNetworks();
+    static QMap<QString, QString> getPhysicalInterfaces();
+    static QList<QString> getCurrentNetworks();
     static QMap<QString, QString> getCurrentIPs();
     static QString getNetwork(QHostAddress ip, QHostAddress netmask);
     static QList<QString> getNetworkIPs(QString network);
