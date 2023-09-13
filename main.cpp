7486fcad975f1d3766bb6edbc6dd5325a28e8d68
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-09-13 20:58:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include "scanner.h"
 
 
 int main(int argc, char *argv[]) {
+    Scanner scanner{};
+    scanner.setNetworksFromFile("D:\\Projects\\QtProjects\\NetworkScanner\\input.txt");
+
     QApplication a(argc, argv);
 
        MainWindow w;
