4b151365a2aaf7e1cf1a3c2532982d13e064f376
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-09-13 19:53:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,12 @@
-#include <QApplication>
-
 #include "mainwindow.h"
-#include "dbmanager.h"
-#include "scanner.h"
+
+#include <QApplication>
 
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
        MainWindow w;
         w.show();
         return a.exec();
-
-//    DBManager dbManager{};
-//    dbManager.chooseTable("test");
-//    dbManager.addNewIP("192.168.1.1");
-
-//    Scanner scanner{};
-//    qInfo() << scanner.getServiceName("1", 1);
-//    scanner.detectActiveHostsSYN();
-//    qInfo() << scanner.getActiveHosts();
-//    scanner.getCurrentNetworks();
-
-//    return 0;
 }
-
-//int main(int argc, char *argv[]) {
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-//    return a.exec();
-//}
