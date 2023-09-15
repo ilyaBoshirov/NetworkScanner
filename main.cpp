5901a21c3a0049404b6104513c7e31871fa48e7e
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-09-15 19:48:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include "scanner.h"
+#include <numeric>
 
 
 int main(int argc, char *argv[]) {
-    Scanner::networksStringIsCorrect("172.16.19.0/24,192.123.0.1/11");
+
 
     QApplication a(argc, argv);
 
