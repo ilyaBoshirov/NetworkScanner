0b5aa945ee0f3854bdac50bac591bc84f3282f8e
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-09-18 11:48:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <numeric>
+#include <QList>
 
 
 int main(int argc, char *argv[]) {
-
-
     QApplication a(argc, argv);
 
        MainWindow w;
